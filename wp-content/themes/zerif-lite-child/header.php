97e97ebbc16a0cd543238c75a6aa3ddef2a55242
Chelsea Overstreet <overstreetchelsea@gmail.com>
2016-05-24 14:55:02 -0400

    Content Fonts

--- wp-content/themes/zerif-lite-child/header.php
+++ wp-content/themes/zerif-lite-child/header.php
@@ -13,6 +13,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href='https://fonts.googleapis.com/css?family=Corben' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 <!--[if lt IE 9]>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/ie.css" type="text/css">
